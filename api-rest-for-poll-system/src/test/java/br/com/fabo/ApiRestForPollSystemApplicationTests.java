7d5402b082562c6e9fa4ee5fe8d762c80a019dba
Fábio Barbosa <92115726+Fabo-Barbosa@users.noreply.github.com>
2025-08-09 11:28:11 -0300

    initialized spring project

---
+++ api-rest-for-poll-system/src/test/java/br/com/fabo/ApiRestForPollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestForPollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
